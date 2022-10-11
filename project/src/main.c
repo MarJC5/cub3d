14bcca0d35b21bbc228501294a12016180e86000
Fabrice Martin <martin@LAPTOP-GTCCACBV.localdomain>
2022-10-11 21:15:15 +0200

    repair de la hess

--- project/src/main.c
+++ project/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmartin <jmartin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 16:08:59 by jmartin           #+#    #+#             */
-/*   Updated: 2022/10/04 11:29:26 by jmartin          ###   ########.fr       */
+/*   Updated: 2022/10/11 21:14:32 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ int	main(int argc, char **argv)
 		init_default(game);
 		if (init_map(game, argv[argc - 1]) == FAILURE)
 			return (EXIT_FAILURE);
+		free_map(game);
 	}
 	else if (argc == 1 || argc > 2)
 		printinvalid(ERR_NAME);
-	free_map(game);
 	free(game);
 	return (EXIT_SUCCESS);
 }
